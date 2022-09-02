e8d3956c860da9074d05e974b873ad4ef1fb25a2
Daniel Alves Rosel <daniel@alves.world>
2022-09-02 11:54:55 +0200

    process roll

--- watch_current_procs.sh
+++ watch_current_procs.sh
@@ -4,9 +4,8 @@ function get_current_proctree() {
     $(python ./backends/process-tree.py > /tmp/procs);
 }
 
-
 while [ 0 ]; do
     $(get_current_proctree)
     cp /tmp/procs /tmp/procs.bak
-    sleep 3;
+    sleep 110;
 done
